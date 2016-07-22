f75a79686351cbfb003925b59cd4a1611b006f11
Julien Dubiel <contact@juliendubiel.net>
2016-07-22 14:32:07 +0200

    Use parallelism again for training.

--- src/process/training/Classifier.java
+++ src/process/training/Classifier.java
@@ -122,16 +122,8 @@ public class Classifier {
 
         // Compute Haar-features of all examples
         HashMap<String, ArrayList<Integer>> result = new HashMap<>();
-        for (ImageHandler image : positives) {
-            if (image.getWidth() == width && image.getHeight() == height) {
-                System.out.println("Image " + image.getFilePath());
-                result.put(image.getFilePath(), computeFeatures(image));
-            }
-            else
-                System.err.println("Image " + image.getFilePath() + " has a wrong size! (Expecting " + width + "x" + height + ", got " + image.getWidth() + "x" + image.getHeight() + ")");
-        }
-//        result.putAll(computeFeaturesImages(positives, width, height));
-//        result.putAll(computeFeaturesImages(negatives, width, height));
+        result.putAll(computeFeaturesImages(positives, width, height));
+        result.putAll(computeFeaturesImages(negatives, width, height));
 
         FeaturesSerializer.toDisk(result, Conf.TRAIN_FEATURES);
     }
